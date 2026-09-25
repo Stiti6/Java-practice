4d6f1ca113b3ffba46cd433436f104e274cf9a6a
Stiti <stitipragyannayak6@gmail.com>
2026-09-25 23:40:08 +0530

    second commit

---
+++ variable.java
@@ -0,0 +1,19 @@
+public class variable{
+    public static void main(String[] args) {
+        int age = 21;
+        double salary = 350000.41;
+        char grade = 'E';
+        boolean isStudent = true;
+        String name = "stiti";
+        long number = 9040233482L;
+
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+                System.out.println("phone number: " + number);
+
+    }
+}
